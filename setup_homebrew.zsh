5d3a38a2b5bbec5bdd92f9dc0df97beffa1d71d1
hamzaerbay <hamzaerbay@gmail.com>
2021-02-24 22:16:36 -0800

    add wget,rectangle and firefox-developer-edition

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,11 +4,13 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install wget
 brew install httpie
 brew install bat
+brew install --cask rectangle
 
 # --no-quarantine disable the gatekeeper
 # Disable/enable quarantining of downloads (default: enabled).
 brew install --no-quarantine google-chrome
+brew install --no-quarantine firefox-developer-edition
 brew install --no-quarantine visual-studio-code
-# brew install --no-quarantine alfred
