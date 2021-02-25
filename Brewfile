a046f306e76f4578bb006e630d93f72582b3e4b8
hamzaerbay <hamzaerbay@gmail.com>
2021-02-24 23:16:54 -0800

    Switch to Brewfile for Homebrew installs

---
+++ Brewfile
@@ -0,0 +1,17 @@
+# Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-versions"
+tap "homebrew/core"
+
+# Brews (Packages)
+brew "bat"
+brew "gh"
+brew "httpie"
+brew "node"
+brew "wget"
+
+# Casks (Applications)
+cask "firefox-developer-edition"
+cask "rectangle"
+cask "visual-studio-code"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,13 +4,16 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install wget
-brew install httpie
-brew install bat
-brew install --cask rectangle
+# brew install wget
+# brew install httpie
+# brew install bat
+# brew install --cask rectangle
 
-# --no-quarantine disable the gatekeeper
-# Disable/enable quarantining of downloads (default: enabled).
-brew install --no-quarantine google-chrome
-brew install --no-quarantine firefox-developer-edition
-brew install --no-quarantine visual-studio-code
+# # --no-quarantine disable the gatekeeper
+# # Disable/enable quarantining of downloads (default: enabled).
+# brew install --no-quarantine google-chrome
+# brew install --no-quarantine firefox-developer-edition
+# brew install --no-quarantine visual-studio-code
+
+# use Brewfile instead of adding above commands
+brew bundle --verbose
